994ca0bf4a192d9c925fd1fd0d896b4e21e38e06
HydraRosario <acevedo_marcos17@hotmail.com>
2023-04-20 16:00:04 -0300

    fc

---
+++ src/components/About.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import CountUp from 'react-countup';
+import { useInView } from 'react-intersection-observer'
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants'
+
+const About = () => {
+  const [ref, inView] = useInView({
+    threshold: 0.5
+  })
+  return (
+    <section className='section' id='about' ref={ref}>
+      <div className='container mx-auto'>
+        <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-y-0 h-screen'>
+          <motion.div
+          variants={fadeIn('right', 0.3)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once: false, amount: 0.3 }}
+          className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'></motion.div>
+          <motion.div
+          variants={fadeIn('left', 0.5 )}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once: false, amount: 0.3 }}
+          className='flex-1'>
+            <h2 className='h2 text-accent'>Sobre mí.</h2>
+            <h3 className='h3 mb-4'>Ofrezco mis servicios de asesoría y consultoría en proyectos turísticos</h3>
+            <p className='mb-6'>
+            Como consultora en proyectos turísticos, mis metas laborales incluyen seguir creciendo en mi carrera profesional y expandiendo mi red de contactos en la industria turística. También me gustaría tener un impacto positivo en la industria turística al promover prácticas sostenibles y responsables en el desarrollo de proyectos turísticos.
+            Me gusta trabajar de manera colaborativa con mis clientes, escuchando sus necesidades y ofreciendo soluciones personalizadas. Me enfoco en construir relaciones sólidas y de confianza con mis clientes y en trabajar juntos hacia el éxito del proyecto. También creo en la importancia de la comunicación clara y efectiva para asegurarme de que todos estemos en la misma página y avanzando en la misma dirección.
+            Además, siempre trato de mantenerme actualizada en las últimas tendencias y novedades en la industria turística para poder brindar el mejor asesoramiento a mis clientes.
+            </p>
+            <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {
+                    inView ?
+                    <CountUp start={0} end={10} duration={3} /> : null}
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Years of <br />
+                  Production
+                </div>
+              </div>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {
+                    inView ?
+                    <CountUp start={0} end={6} duration={3} /> : null}
+                    k+
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Hours of <br />
+                  Coding
+                </div>
+              </div>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {
+                    inView ?
+                    <CountUp start={650} end={700} duration={1} /> : null}
+                    +
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Satisfied <br />
+                  Customers
+                </div>
+              </div>
+              <div className='flex gap-x-8 items-center'>
+              <button className='btn btn-lg'>
+                <a href='https://www.linkedin.com/in/HydraRosario/'>
+                  Contact me
+                </a>
+              </button>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  )
+};
+
+export default About;
---
+++ src/components/Banner.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import Image from '../assets/avatar2.png';
+import { BsInstagram } from 'react-icons/bs';
+import { TypeAnimation } from 'react-type-animation';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants'
+
+const Banner = () => {
+  return (
+    <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
+          <div className='flex-1 text-center font-secondary lg:text-left'>
+            <motion.h1
+              variants={fadeIn('up', 0.3)}
+              initial='hidden' whileInView={'show'}
+              viewport={{once: false, amount: 0.7}}
+              className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'
+            >
+              Andrea <span>Ventancor</span>
+            </motion.h1>
+            <motion.div
+              variants={fadeIn('up', 0.4)}
+              initial='hidden' whileInView={'show'}
+              viewport={{once: false, amount: 0.7}}
+              className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'
+            >
+              <span className='text-white mr-4'>-</span>
+              <TypeAnimation sequence={[
+                'Asesoria',
+                2000,
+                'Turismo',
+                2000,
+                'Periodismo',
+                2000
+              ]}
+              speed={50}
+              className='text-accent'
+              wrapper='span'
+              repeat={Infinity}
+              />
+            </motion.div>
+            <motion.p
+              variants={fadeIn('up', 0.5)}
+              initial='hidden' whileInView={'show'}
+              viewport={{once: false, amount: 0.7}}
+              className='mb-8 max-w-lg mx-auto lg:mx-0'
+            >
+            Consultora en proyectos turísticos con experiencia en planificación y desarrollo sostenible. Creo experiencias únicas y memorables para los viajeros, maximizando el impacto positivo en la comunidad local. Ayudo a llevar tu proyecto al siguiente nivel mediante la identificación de oportunidades y la implementación de soluciones efectivas. ¡Hablemos!
+            </motion.p>
+            <motion.div
+              variants={fadeIn('up', 0.6)}
+              initial='hidden' whileInView={'show'}
+              viewport={{once: false, amount: 0.7}}
+              className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'
+            >
+              <button className='btn btn-sm'>
+                <a href='https://www.facebook.com/andrea.ventancor'>Contactame</a>
+              </button>
+              {/* <button className='btn btn-lg'>
+              <a href='https://www.youtube.com/@HydraRosario'>
+                <FaYoutube/>
+              </a>
+              </button>
+              <button className='btn btn-lg'>
+              <a href='https://github.com/HydraRosario'>
+                <FaGithub/>
+              </a>
+              </button> */}
+              <a className='btn btn-lg' href='https://www.instagram.com/aventancor'><BsInstagram/></a>
+            </motion.div>
+            <motion.div
+              variants={fadeIn('up', 0.7)}
+              initial='hidden' whileInView={'show'}
+              viewport={{once: false, amount: 0.7}}
+              className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'
+            >
+            </motion.div>
+          </div>
+          <motion.div
+            variants={fadeIn('down', 0.5)}
+            initial='hidden' whileInView={'show'}
+            className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]'>
+              <img src={Image} alt='' height='600' width='512'/>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  )
+};
+
+export default Banner;
